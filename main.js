bf929a578133e60a1e437e4c8a2b3c3a91717090
shoshinobi <30033805+shoshinobi@users.noreply.github.com>
2022-10-23 11:34:24 +0300

    next: planet creation function

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import './style.css'
+//import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
@@ -59,9 +59,8 @@ window.addEventListener('resize', () => {
 /*-------------------------------- Camera ------------------------------*/
 
 
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.z = 5
-camera.position.y = 1
+const camera = new THREE.PerspectiveCamera(40, sizes.width / sizes.height, 0.1, 1000)
+camera.position.set(-90, 140, 140)
 scene.add(camera)
 
 /*-------------------------------- Controls ------------------------------*/
@@ -77,11 +76,13 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-renderer.outputEncoding = THREE.sRGBEncoding
 
 /*-------------------------------- Lights ------------------------------*/
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.15)
 scene.add(ambientLight) 
+
+const sunLight = new THREE.PointLight(0xffffff, 2, 300)
+scene.add(sunLight)
 
 /*-------------------------------- Background ------------------------------*/
 
@@ -100,8 +101,39 @@ scene.background = backgroundTexture
 
 /*================================ Characters ==============================*/
 
-const planet = new THREE.Mesh(new THREE.SphereGeometry(1, 32, 32), new THREE.MeshStandardMaterial({ color: 0xff0000 }))
-scene.add(planet)
+// Sun
+const sunGeo = new THREE.SphereGeometry(16, 32 , 32)
+const sunMat = new THREE.MeshBasicMaterial({map: sunTex})
+const sun = new THREE.Mesh(sunGeo, sunMat)
+scene.add(sun)
+
+// Mars
+const marsOrbit = new THREE.Object3D()
+scene.add(marsOrbit)
+const marsGeo = new THREE.SphereGeometry(3, 32 , 32)
+const marsMat = new THREE.MeshStandardMaterial({map: marsTex})
+const mars = new THREE.Mesh(marsGeo, marsMat)
+marsOrbit.add(mars)
+mars.position.set(40, 0, 0)
+
+// Saturn
+const saturnOrbit = new THREE.Object3D()
+scene.add(saturnOrbit)
+const saturnGeo = new THREE.SphereGeometry(8, 32 , 32)
+const saturnMat = new THREE.MeshStandardMaterial({map: saturnTex})
+const saturn = new THREE.Mesh(saturnGeo, saturnMat)
+saturnOrbit.add(saturn)
+saturn.position.set(140, 0, 0)
+
+// Saturn ring
+const saturnRingGeo = new THREE.RingGeometry(10, 20, 32)
+const saturnRingMat = new THREE.MeshStandardMaterial({map: saturnRingTex, side: THREE.DoubleSide})
+const saturnRing = new THREE.Mesh(saturnRingGeo, saturnRingMat)
+saturnRing.rotation.x = Math.PI * 0.5
+saturn.add(saturnRing)
+
+
+
 
 
 
@@ -111,6 +143,21 @@ scene.add(planet)
 const clock = new THREE.Clock()
 const tick = () => {
     const elapsedTime = clock.getElapsedTime()
+    
+    // Animation
+    sun.rotation.y = elapsedTime * 0.1
+    marsOrbit.rotation.y = elapsedTime * 0.3
+    mars.rotation.y = elapsedTime * 0.4
+    saturnOrbit.rotation.y = elapsedTime * 0.05
+    saturn.rotation.y = elapsedTime * -0.3
+    
+    
+    
+    
+    
+    
+    
+    
     controls.update()
     renderer.render(scene, camera)
     window.requestAnimationFrame(tick)
